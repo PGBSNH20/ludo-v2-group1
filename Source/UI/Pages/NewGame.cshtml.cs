99f7147dde373d59a365ce65930d71ccfb7d7b15
Svetlana Ernyleva <70198472+SvetlanaErn@users.noreply.github.com>
2021-05-19 11:35:42 +0200

    fixes duplicates of game names in DB

--- Source/UI/Pages/NewGame.cshtml.cs
+++ Source/UI/Pages/NewGame.cshtml.cs
@@ -27,12 +27,20 @@ namespace Ludo.Web.Pages
         {
             if (!ModelState.IsValid)
                 return Page();
+            // Search for a game named Board.BoardName in the DB
+            var boardInDB = await _ludoData.GetBoardAsync(Board.BoardName);
+            
+            // if there is no game with this name, create it
+            if (boardInDB == null)
+            {
+                var response = await _ludoData.AddBoard(Board.BoardName);
 
-            var response = await _ludoData.AddBoard(Board.BoardName);
-
-            if (response.IsSuccessStatusCode)
-                return RedirectToPage("Player", new { gameName = Board.BoardName });
-            return Page();
+                if (response.IsSuccessStatusCode)
+                    return RedirectToPage("Player", new { gameName = Board.BoardName });
+                return Page();
+            }
+            
+            return RedirectToPage("Player", new { gameName = Board.BoardName });
         }
     }
 }
