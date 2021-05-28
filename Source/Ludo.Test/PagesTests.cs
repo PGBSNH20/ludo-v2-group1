5b07e42618bee2df57638482f2b6c53e27f9106b
Svetlana Ernyleva <70198472+SvetlanaErn@users.noreply.github.com>
2021-05-28 18:00:39 +0200

    more Page tests

--- Source/Ludo.Test/PagesTests.cs
+++ Source/Ludo.Test/PagesTests.cs
@@ -6,11 +6,13 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using FakeItEasy;
+using Ludo.API.Models;
 using Ludo.Web;
 using Ludo.Web.Pages;
 using LudoAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestSharp;
 using Xunit;
 
 namespace Ludo.Test
@@ -18,7 +20,7 @@ namespace Ludo.Test
     public class PagesTests
     {
         [Fact]
-        public async Task OnGetAsync_Populates_PageModel_With_BoardAsync()
+        public async Task Game_OnGetAsync_Populates_PageModel_With_BoardAsync()
         {
             var fakeBoard = new Board { BoardName = "TestBoard" };
             var ludoData = A.Fake<ILudoDataAccess>();
@@ -32,7 +34,7 @@ namespace Ludo.Test
         }
 
         [Fact]
-        public async Task OnPostAsync_ReturnsA_RedirectTo_PageResult()
+        public async Task NewGame_OnPostAsync_ReturnsA_RedirectTo_PageResult()
         {
             var ludoData = A.Fake<ILudoDataAccess>();
             var pageModel = new NewGameModel(ludoData);
@@ -49,7 +51,7 @@ namespace Ludo.Test
             Assert.IsType<RedirectToPageResult>(result);
         }
         [Fact]
-        public async Task OnPostAsync_BadRequest_Returns_Page()
+        public async Task NewGame_OnPostAsync_BadRequest_Returns_Page()
         {
             var ludoData = A.Fake<ILudoDataAccess>();
             var pageModel = new NewGameModel(ludoData);
@@ -62,6 +64,66 @@ namespace Ludo.Test
                 .Returns(new HttpResponseMessage { StatusCode = HttpStatusCode.BadRequest });
 
             var result = await pageModel.OnPostAsync();
+
+            Assert.IsType<RedirectToPageResult>(result);
+        }
+        [Fact]
+        public async Task Player_OnPostAddPlayerAsync_Returns_PageResult()
+        {
+            var ludoData = A.Fake<ILudoDataAccess>();
+            var pageModel = new PlayerModel(ludoData);
+            Board board = new Board { BoardName = "TestBoard", Id = 1 };
+            PlayerTokenColor player = new PlayerTokenColor { PlayerName = "Bob", TokenColor = "Red" };
+            pageModel.Board = board;
+            pageModel.NameColor = player;
+            A.CallTo(() => ludoData.PostPlayer("TestBoard", player)).Returns(new RestResponse { StatusCode = HttpStatusCode.OK });
+            A.CallTo(() => ludoData.GetGameAsync("Testboard")).Returns(board);
+
+            var result = await pageModel.OnPostAddPlayerAsync("TestBoard");
+
+            Assert.IsType<PageResult>(result);
+        }
+        [Fact]
+        public async Task Player_OnPostAddPlayerAsync_Bad_Game_Name_Returns_RedirectPageResult()
+        {
+            var ludoData = A.Fake<ILudoDataAccess>();
+            var pageModel = new PlayerModel(ludoData);
+            Board board = new Board { BoardName = "BadGameName", Id = 1 };
+            PlayerTokenColor player = new PlayerTokenColor { PlayerName = "Bob", TokenColor = "Red" };
+            pageModel.Board = board;
+            pageModel.NameColor = player;
+            Board BadBoard = null;  
+            A.CallTo(() => ludoData.PostPlayer("BadGameName", player)).Returns(new RestResponse { StatusCode = HttpStatusCode.BadRequest });
+            A.CallTo(() => ludoData.GetGameAsync("BadGameName")).Returns(BadBoard);
+
+            var result = await pageModel.OnPostAddPlayerAsync("BadGameName");
+
+            Assert.IsType<RedirectToPageResult>(result);
+        }
+        [Fact]
+        public async Task Player_OnGetAsync_Returns_PageResult()
+        {
+            var ludoData = A.Fake<ILudoDataAccess>();
+            var pageModel = new PlayerModel(ludoData);
+            Board board = new Board { BoardName = "TestBoard", Id = 1 };
+            pageModel.Board = board;
+            A.CallTo(() => ludoData.GetGameAsync("TestBoard")).Returns(board);
+
+            var result = await pageModel.OnPostAddPlayerAsync("TestBoard");
+
+            Assert.IsType<PageResult>(result);
+        }
+        [Fact]
+        public async Task Player_OnGetAsync_Bad_Game_Name_Returns_RedirectPageResult()
+        {
+            var ludoData = A.Fake<ILudoDataAccess>();
+            var pageModel = new PlayerModel(ludoData);
+            Board board = new Board { BoardName = "BadGameName", Id = 1 };
+            pageModel.Board = board;
+            Board BadBoard = null;
+            A.CallTo(() => ludoData.GetGameAsync("BadGameName")).Returns(BadBoard);
+
+            var result = await pageModel.OnPostAddPlayerAsync("BadGameName");
 
             Assert.IsType<RedirectToPageResult>(result);
         }
